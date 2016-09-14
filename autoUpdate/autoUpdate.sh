1ab0fe4978101bce36e4499a6fa7c899f66fbfda
Karsten Nilsen <karsten@cleverit.nl>
2016-09-14 17:27:48 +0200

    Add linux autoUpdate script

---
+++ autoUpdate/autoUpdate.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+../firewall/firewall-enable
+
+#Server zonder spaties
+servert=$HOSTNAME
+
+
+err=0
+subject=""
+from="autoUpdate-"$servert"@cleverit.nl"
+
+apt-get update
+if [[ $? > 0 ]]
+then
+        err=1
+fi
+
+apt-get -y upgrade
+if [[ $? > 0 ]]
+then
+        err=1
+fi
+
+apt-get autoclean
+if [[ $? > 0 ]]
+then
+        err=1
+fi
+
+if [[ $err > 0 ]]
+then
+    subject="ERROR - HELP linux autoUpdate voor server: $servert is mislukt!!!"
+    echo "HELP ER IS EEN FOUT!!!" > ./email.txt
+    echo "Server: $servert" > ./email.txt
+else
+    subject="HOERA - Linux autoUpdate voor server: $servert is succesvol!!!"
+    echo "HOERA het is GOED!!!" > ./email.txt
+    echo "Server: $servert" > ./email.txt
+fi
+
+
+cat ./updatelog.log >> ./email.txt
+cat ./email.txt | mail -aFrom:"autoUpdate-"$servert"@cleverit.nl" -s "$subject" backup@cleverit.nl
