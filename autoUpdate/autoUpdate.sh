9ad96e1ee691d6bc39474874a6b95ffb09d5ee76
Karsten Nilsen <karsten@cleverit.nl>
2020-09-25 10:36:19 +0200

    Update van CDK

--- autoUpdate/autoUpdate.sh
+++ autoUpdate/autoUpdate.sh
@@ -33,13 +33,14 @@ if [[ $err > 0 ]]
 then
     subject="ERROR - HELP linux autoUpdate voor server: $servert is mislukt!!!"
     echo "HELP ER IS EEN FOUT!!!" > ./email.txt
-    echo "Server: $servert" > ./email.txt
+    echo "Server: $servert" >> ./email.txt
 else
     subject="HOERA - Linux autoUpdate voor server: $servert is succesvol!!!"
     echo "HOERA het is GOED!!!" > ./email.txt
-    echo "Server: $servert" > ./email.txt
+    echo "Server: $servert" >> ./email.txt
 fi
 
+cat /etc/*-release >> ./email.txt
 
 cat ./updatelog.log >> ./email.txt
 cat ./email.txt | mail -aFrom:"autoUpdate-"$servert"@cleverit.nl" -s "$subject" backup@cleverit.nl
